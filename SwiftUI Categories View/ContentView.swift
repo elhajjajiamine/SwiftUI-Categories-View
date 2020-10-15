da4292f29cd6143a1973fa5f544b1a4a30320554
elhajjajiamine <aminselhajjaji@gmail.com>
2020-10-15 19:20:42 +0100

    Initial Commit

---
+++ SwiftUI Categories View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Categories View
+//
+//  Created by elhajjaji on 10/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Categories View/SwiftUI_Categories_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Categories_ViewApp.swift
+//  SwiftUI Categories View
+//
+//  Created by elhajjaji on 10/15/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Categories_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
